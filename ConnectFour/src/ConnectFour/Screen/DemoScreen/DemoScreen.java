5969206a17f1c4377153541bf4f2441520d8d75e
masanari0812 <sugomasa4649@gmail.com>
2023-06-06 22:54:14 +0900

    Demo
    あ

--- ConnectFour/src/ConnectFour/Screen/DemoScreen/DemoScreen.java
+++ ConnectFour/src/ConnectFour/Screen/DemoScreen/DemoScreen.java
@@ -9,27 +9,55 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
+//デモ用のスクリーンなので完成版には使用しない
+//OriginScreenを継承している
 public class DemoScreen extends OriginScreen {
-
+	//OriginScreenを継承しているので下の変数は記述していないが存在する。
+	//protected Scene scene;
+	
+	//結果を表示するTextField
+	//メンバ変数としておかないとClickButtonイベントがアクセスできない
 	private TextField tf;
+	
 	public DemoScreen() {
+		//複数のNodeを盾に結合できるVBoxを生成
 		VBox vb=new VBox();
+		
+		//(ButtonやTextなどの)Nodeの感覚を20pxに設定する。
 		vb.setSpacing(20);
+		//1から5までのテキストを作りClickButtonイベントを設定する。
 		for(int i=1;i<=5;i++) {
+			//Textに表示する文字列の作成
 			String str=String.valueOf(i)+"番目のボタン";
+			//Textインスタンス生成
 			Text text=new Text(str);
+			//作成したインスタンスにクリックしたときのイベントを設定
 			text.setOnMouseClicked(new ClickButton(i));
+			//VBoxに作成したNodeを追加(今回はTextインスタンス)
 			vb.getChildren().add(text);
 		}
+		//メンバ変数tfにTextFieldインスタンスを生成し代入
 		this.tf=new TextField();
+		
+		//VBoxに作成したNodeを追加(今回はTextFieldインスタンス)
 		vb.getChildren().add(tf);
+		
+		//作成したVBoxをもとにSceneインスタンスを生成
+		//代入先はこのクラスの継承元OriginScreenのメンバ変数scene
 		scene = new Scene(vb);
-		ConnectFour.getStage().setScene(scene);
+		//画面切り替え処理
+		changeNextScreen();
+		
 	}
-
+	
+	//スクリーン転換時の処理
+	//(今回はデモなのでスクリーン転換ではなくスクリーン設定のみになっている)
 	@Override
 	public void changeNextScreen() {
-
+		
+		//ConnctFour.getStage()で起動中のStageを持ってこれる
+		//持ってきたStageに作成したSceneを設定(この時点で画面が切り替わる)
+		ConnectFour.getStage().setScene(scene);
 	}
 	
 	class ClickButton implements EventHandler<MouseEvent> {
--- ConnectFour/src/ConnectFour/Screen/OriginScreen.java
+++ ConnectFour/src/ConnectFour/Screen/OriginScreen.java
@@ -3,7 +3,11 @@ package ConnectFour.Screen;
 import javafx.scene.Scene;
 
 public abstract class  OriginScreen  {
+	//各スクリーンを格納する変数
 	protected Scene scene;
+	
+	//スクリーン転換時の処理
+	//(例:SelectModeScreenからPlayGameScreenへの画面切り替え処理)
 	public abstract void changeNextScreen();
 	
 }
