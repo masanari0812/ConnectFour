56c11c3534d04b5a4f32983ffa3ee793b12d3198
masanari0812 <sugomasa4649@gmail.com>
2023-06-20 16:07:10 +0900

    a

--- ConnectFour/src/ConnectFour/ConnectFour.java
+++ ConnectFour/src/ConnectFour/ConnectFour.java
@@ -1,31 +1,30 @@
 package ConnectFour;
 
-import java.util.HashMap;
-
-import ConnectFour.Screen.OriginScreen;
+import ConnectFour.Screen.PlayGameScreen.GameManager;
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class ConnectFour extends Application {
 	private static Stage stage;
+	private static GameManager gm;
 	
 	public static void main(String[] args) {
 		launch(args);
-
 	}
 
 	@Override
 	public void start(Stage stage) throws Exception {
 		setStage(stage);
 		
-		
-		HashMap<String,OriginScreen> sc;
-		
-		
-		stage.setTitle("ConnectFourDemo");
+		this.gm=new GameManager(7,6);
+		stage.setTitle("ConnectFour");
 		stage.show();
 	}
 
+	public static void setScene(Scene scene) {
+		getStage().setScene(scene);
+	}
 	public static void setStage(Stage stage) {
 		ConnectFour.stage = stage;
 	}
--- ConnectFour/src/ConnectFour/Screen/PlayGameScreen/GameManager.java
+++ ConnectFour/src/ConnectFour/Screen/PlayGameScreen/GameManager.java
@@ -2,12 +2,23 @@ package ConnectFour.Screen.PlayGameScreen;
 
 import java.util.ArrayList;
 import java.util.List;
-//import javafx.event.EventHandler;
+
+import ConnectFour.ConnectFour;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 
 public class GameManager {
 	private List<List<PlayerAffiliation>> boardState;
 	private int column, row;
-	
+	private HBox hb;
+
 	public GameManager(int column, int row) {
 		boardState = new ArrayList<>();
 		for (int x = 0; x < column; x++) {
@@ -15,32 +26,69 @@ public class GameManager {
 		}
 		this.column = column;
 		this.row = row;
+		reloadBoard();
 	}
-	
+
 	public PlayerAffiliation putOnSpace(PlayerAffiliation player, int x) {
 		if (boardState.get(x).size() <= row) {
 			boardState.get(x).add(player);
 		}
 		return PlayerAffiliation.NONE;
 	}
-	
+
 	public PlayerAffiliation getSpace(int x, int y) {
 		if (x < 0 || x >= column || y < 0 || y >= boardState.get(x).size()) {
 			return PlayerAffiliation.NONE;
 		}
 		return boardState.get(x).get(y);
 	}
-	
-	public int getFirstNoneSpace(int x) {
-		return boardState.get(x).size() - 1;
+
+	public void setSpace(PlayerAffiliation player, int x, int y) {
+		if (player == PlayerAffiliation.NONE)
+			return;
+		boardState.get(x).remove(y);
+		boardState.get(x).add(y, player);
+		reloadBoard();
 	}
-	
-	/*
-	public class activateSkill implements EventHandler<BottunEvent> {
-		
-	} */
-	
-	
+
+	public void setSpace(PlayerAffiliation player, int x) {
+		if (player == PlayerAffiliation.NONE)
+			return;
+		boardState.get(x).add(player);
+		reloadBoard();
+	}
+
+	public void reloadBoard() {
+		HBox hb = new HBox();
+		hb.setSpacing(5);
+		for (int x = 0; x < column; x++) {
+			VBox vb;
+			hb.getChildren().add(vb = new VBox());
+			vb.setSpacing(5);
+			for (int y = row - 1; y >= 0; y--) {
+				//for (int y = 0; y < row; y++) {
+				Circle space = new Circle(40, 50, 50); // space:マス radius:30
+				space.setFill(getSpace(x, y).getColor());
+				space.setOnMousePressed(new ClickBoardEventHandler(x, y));
+				vb.getChildren().add(space);
+			}
+		}
+
+		VBox sideBar = new VBox();
+		Button bt = new Button("Skill");
+		bt.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+		Rectangle r = new Rectangle(40, 50, 120, 360);
+		r.setFill(Color.GREY);
+		sideBar.getChildren().addAll(r, bt);
+		hb.getChildren().add(sideBar);
+		Scene sc = new Scene(hb);
+		ConnectFour.getStage().setScene(sc);
+	}
+
+	public int getFirstNoneSpace(int x) {
+		return boardState.get(x).size();
+	}
+
 	// 盤面の把握
 	public int countColumnSpace(PlayerAffiliation team) {
 		int count, point = 0;
@@ -49,8 +97,7 @@ public class GameManager {
 			for (int y = 0; y < row; y++) {
 				if (getSpace(x, y).equals(team)) {
 					count++;
-				}
-				else {
+				} else {
 					count = 0;
 				}
 				if (count >= 4) {
@@ -61,7 +108,7 @@ public class GameManager {
 		}
 		return point;
 	}
-	
+
 	public int countRowSpace(PlayerAffiliation team) {
 		int count, point = 0;
 		for (int y = 0; y < row; y++) {
@@ -69,8 +116,7 @@ public class GameManager {
 			for (int x = 0; x < column; x++) {
 				if (getSpace(x, y).equals(team)) {
 					count++;
-				}
-				else {
+				} else {
 					count = 0;
 				}
 				if (count >= 4) {
@@ -81,16 +127,15 @@ public class GameManager {
 		}
 		return point;
 	}
-	
+
 	public int countRightSlashSpace(PlayerAffiliation team) {
 		int count, point = 0;
 		for (int x = 0; x < column; x++) {
 			count = 0;
 			for (int y = 0; y < row; y++) {
-				if (getSpace(x+y, y).equals(team)) {
+				if (getSpace(x + y, y).equals(team)) {
 					count++;
-				}
-				else {
+				} else {
 					count = 0;
 				}
 				if (count >= 4) {
@@ -101,16 +146,15 @@ public class GameManager {
 		}
 		return point;
 	}
-	
+
 	public int countLeftSlashSpace(PlayerAffiliation team) {
 		int count, point = 0;
 		for (int x = 0; x < column; x++) {
 			count = 0;
 			for (int y = 0; y < row; y++) {
-				if (getSpace(column-x-y-1, y).equals(team)) {
+				if (getSpace(column - x - y - 1, y).equals(team)) {
 					count++;
-				}
-				else {
+				} else {
 					count = 0;
 				}
 				if (count >= 4) {
@@ -121,14 +165,37 @@ public class GameManager {
 		}
 		return point;
 	}
-	
+
 	public boolean breakSpace(int x, int y) {
 		if (getSpace(x, y).equals(PlayerAffiliation.NONE)) {
 			return false;
-		}
-		else {
+		} else {
 			return true;
 		}
 	}
-		
+
+	// マウスでマスをクリックしたら赤or黄色に染まる処理を追加する.
+	class ClickBoardEventHandler implements EventHandler<MouseEvent> {
+		private int x;
+		private int y;
+
+		public ClickBoardEventHandler(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		public ClickBoardEventHandler(int x) {
+			this.x = x;
+		}
+
+		@Override
+		public void handle(MouseEvent e) {
+			if (e.isPrimaryButtonDown())
+				setSpace(PlayerAffiliation.PLAYER1, x);
+			else if (e.isSecondaryButtonDown())
+				setSpace(PlayerAffiliation.PLAYER2, x);
+			System.out.println(String.valueOf(x) + " " + String.valueOf(y));
+
+		}
+	}
 }
