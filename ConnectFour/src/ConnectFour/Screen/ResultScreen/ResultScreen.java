51662f0f04f7d845e641ff6dc5dd1e52fbab2013
al21095 <134358132+al21095@users.noreply.github.com>
2023-06-21 23:14:13 +0900

    a

--- ConnectFour/src/ConnectFour/Screen/ResultScreen/ResultScreen.java
+++ ConnectFour/src/ConnectFour/Screen/ResultScreen/ResultScreen.java
@@ -21,6 +21,7 @@ public class ResultScreen extends OriginScreen {
 			button = new Button("続ける");
 			button1 = new Button("モード選択");
 			button.setPrefWidth(50);
+			button.setPrefHeight(50);
 		    BorderPane bp = new BorderPane();
 		    bp.setLeft(button);
 		    bp.setRight(button1);
