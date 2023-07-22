07eb09a5bb19afc15d77f3ea85751ee4a991dd18
al21075 <al21075@shibaura-it.ac.jp>
2023-07-22 21:49:30 +0900

    PlayerAffiliationとPlayGameScreenの変更

--- ConnectFour/src/ConnectFour/Screen/PlayGameScreen/PlayerAffiliation.java
+++ ConnectFour/src/ConnectFour/Screen/PlayGameScreen/PlayerAffiliation.java
@@ -1,33 +1,58 @@
+/*******************************************************************
+*** File Name             : PlayerAffiliation.java
+*** Designer              : 塚田 大貴
+*** Date                  : 2023.07.22
+*** Purpose               : 埋まっていないマス，プレイヤー，スキルブロックを識別する処理を記述する
+***
+*******************************************************************/
+
 package ConnectFour.Screen.PlayGameScreen;
 
 import javafx.scene.paint.Color;
 
-// プレイヤーを識別する処理
 public enum PlayerAffiliation {
 	NONE(0), PLAYER1(1), PLAYER2(2), BLOCK(3);
 	
 	final private Color color;
-	
+
+	/****************************************************************************
+	*** Method Name         : PlayerAffiliation(int i)
+	*** Designer            : 塚田 大貴
+	*** Date                : 2023.07.22
+	*** Function            : 埋まっていないマスをColor.GREY，PLAYER1をColor.RED，PLAYER2をColor.YELLOW，
+	                          スキルブロックをColor.BLUE，例外をColor.MEDIUMPURPLEとしてcolorに格納
+	*** Return              : なし
+	                          
+	****************************************************************************/	
+
 	PlayerAffiliation(int i) {
 		switch(i) {
 		
 		case 0 :
-			color = Color.GREY;				// 埋まっていないマス
+			color = Color.GREY;
 			break;
 		case 1 :
-			color = Color.RED;				// PLAYER1は赤いマス
+			color = Color.RED;
 			break;
 		case 2 :
-			color = Color.YELLOW;			// PLAYER2は黄色いマス
+			color = Color.YELLOW;
 			break;
 		case 3 :
-			color = Color.BLUE;				// スキルのブロックの色
+			color = Color.BLUE;
 			break;
 		default :
-			color = Color.MEDIUMPURPLE;	// 例外
+			color = Color.MEDIUMPURPLE;
 			break;
 		}
 	}
+	
+	/****************************************************************************
+	*** Method Name         : getColor()
+	*** Designer            : 塚田 大貴
+	*** Date                : 2023.07.22
+	*** Function            : colorを返り値として返す
+	*** Return              : color 色情報
+	****************************************************************************/
 	
 	Color getColor() {
 		return color;
