88867575b7fcf360879b41fcbfea3cbb93223d17
masanari0812 <sugomasa4649@gmail.com>
2023-07-22 22:01:27 +0900

    Revert "Merge branch 'intro' of https://github.com/masanari0812/Test11 into intro"
    This reverts commit e3de3d6a0cfe727a706c4e1b3746df3b2fee2aeb, reversing
    changes made to e4a2dabb470a4656efce0cbcbe4960623cb76000.

--- ConnectFour/src/ConnectFour/Screen/PlayGameScreen/PlayerAffiliation.java
+++ ConnectFour/src/ConnectFour/Screen/PlayGameScreen/PlayerAffiliation.java
@@ -1,58 +1,33 @@
-/*******************************************************************
-*** File Name             : PlayerAffiliation.java
-*** Designer              : 塚田 大貴
-*** Date                  : 2023.07.22
-*** Purpose               : 埋まっていないマス，プレイヤー，スキルブロックを識別する処理を記述する
-***
-*******************************************************************/
-
 package ConnectFour.Screen.PlayGameScreen;
 
 import javafx.scene.paint.Color;
 
+// プレイヤーを識別する処理
 public enum PlayerAffiliation {
 	NONE(0), PLAYER1(1), PLAYER2(2), BLOCK(3);
 	
 	final private Color color;
-
-	/****************************************************************************
-	*** Method Name         : PlayerAffiliation(int i)
-	*** Designer            : 塚田 大貴
-	*** Date                : 2023.07.22
-	*** Function            : 埋まっていないマスをColor.GREY，PLAYER1をColor.RED，PLAYER2をColor.YELLOW，
-	                          スキルブロックをColor.BLUE，例外をColor.MEDIUMPURPLEとしてcolorに格納
-	*** Return              : なし
-	                          
-	****************************************************************************/	
-
+	
 	PlayerAffiliation(int i) {
 		switch(i) {
 		
 		case 0 :
-			color = Color.GREY;
+			color = Color.GREY;				// 埋まっていないマス
 			break;
 		case 1 :
-			color = Color.RED;
+			color = Color.RED;				// PLAYER1は赤いマス
 			break;
 		case 2 :
-			color = Color.YELLOW;
+			color = Color.YELLOW;			// PLAYER2は黄色いマス
 			break;
 		case 3 :
-			color = Color.BLUE;
+			color = Color.BLUE;				// スキルのブロックの色
 			break;
 		default :
-			color = Color.MEDIUMPURPLE;
+			color = Color.MEDIUMPURPLE;	// 例外
 			break;
 		}
 	}
-	
-	/****************************************************************************
-	*** Method Name         : getColor()
-	*** Designer            : 塚田 大貴
-	*** Date                : 2023.07.22
-	*** Function            : colorを返り値として返す
-	*** Return              : color 色情報
-	****************************************************************************/
 	
 	Color getColor() {
 		return color;
